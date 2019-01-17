9b1cd70af5d3a5b565d51f43bfe2234ec01ddf13
Simon Willison <swillison@gmail.com>
2019-01-16 21:09:05 -0800

    Applied black code formatting standards

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,60 +3,52 @@ import pytest
 import sqlite3
 import pandas as pd
 
-TEST_TABLES = '''
+TEST_TABLES = """
 CREATE TABLE foo (
   id integer primary key,
   value text
 );
-'''
+"""
 
 
-@pytest.mark.parametrize('table,expected', [
-    ('foo', True),
-    ('bar', False),
-])
+@pytest.mark.parametrize("table,expected", [("foo", True), ("bar", False)])
 def test_table_exists(table, expected):
-    conn = sqlite3.connect(':memory:')
+    conn = sqlite3.connect(":memory:")
     conn.executescript(TEST_TABLES)
     assert expected == utils.table_exists(conn, table)
 
 
 def test_get_create_table_sql():
-    df = pd.DataFrame([{'number': 1, 'letter': 'a'}])
-    sql, columns = utils.get_create_table_sql('hello', df)
+    df = pd.DataFrame([{"number": 1, "letter": "a"}])
+    sql, columns = utils.get_create_table_sql("hello", df)
     assert (
         'CREATE TABLE "hello" (\n'
         '"index" INTEGER,\n'
         '  "letter" TEXT,\n'
         '  "number" INTEGER\n'
-        ')'
+        ")"
     ) == sql
-    assert ['index', 'letter', 'number'] == columns
+    assert ["index", "letter", "number"] == columns
 
 
 def test_refactor_dataframes():
-    df = pd.DataFrame([{
-        'name': 'Terry',
-        'score': 0.5,
-    }, {
-        'name': 'Terry',
-        'score': 0.8,
-    }, {
-        'name': 'Owen',
-        'score': 0.7,
-    }])
-    conn = sqlite3.connect(':memory:')
-    output = utils.refactor_dataframes(conn, [df], {'name': ('People', 'first_name')}, False)
+    df = pd.DataFrame(
+        [
+            {"name": "Terry", "score": 0.5},
+            {"name": "Terry", "score": 0.8},
+            {"name": "Owen", "score": 0.7},
+        ]
+    )
+    conn = sqlite3.connect(":memory:")
+    output = utils.refactor_dataframes(
+        conn, [df], {"name": ("People", "first_name")}, False
+    )
     assert 1 == len(output)
     dataframe = output[0]
     # There should be a 'People' table in sqlite
-    assert [
-        (1, 'Terry'),
-        (2, 'Owen'),
-    ] == conn.execute('select id, first_name from People').fetchall()
+    assert [(1, "Terry"), (2, "Owen")] == conn.execute(
+        "select id, first_name from People"
+    ).fetchall()
     assert (
-        '   name  score\n'
-        '0     1    0.5\n'
-        '1     1    0.8\n'
-        '2     2    0.7'
+        "   name  score\n" "0     1    0.5\n" "1     1    0.8\n" "2     2    0.7"
     ) == str(dataframe)
