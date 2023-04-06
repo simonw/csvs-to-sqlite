fcd5b9c7485bc7b95bf2ed9507f18a60728e0bcb
Luighi <luighiavz@gmail.com>
2023-04-05 23:55:35 -0300

    Replace deprecated argument `error_bad_lines` for pandas
    With new pandas version 2.0.0, the argument was removed, retrieving an
    error.
    
    Reference: https://pandas.pydata.org/docs/whatsnew/v2.0.0.html#removal-of-prior-version-deprecations-changes

--- csvs_to_sqlite/utils.py
+++ csvs_to_sqlite/utils.py
@@ -39,7 +39,7 @@ def load_csv(
                     filepath,
                     sep=separator,
                     quoting=quoting,
-                    error_bad_lines=not skip_errors,
+                    on_bad_lines='skip' if skip_errors else 'error',
                     low_memory=True,
                     encoding=encoding,
                     usecols=usecols,
